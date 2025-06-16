72e1b8205fdabf4c6ff7ae3725277862ab552896
Mariaclara728 <amandateixeiramakeup@gmail.com>
2025-06-16 20:36:40 -0300

    hhdh

--- lib/quiz-data.ts
+++ lib/quiz-data.ts
@@ -238,6 +238,21 @@ export const quizSteps = [
     additionalImage2: "https://comprarplanseguro.shop/wp-content/uploads/2025/06/2-LUIS-DEP-CANVA.png"
   },
   {
+    id: 12,
+    question: "RESULTADOS DEMOSTRADOS",
+    subtext:
+      "EL 91% DE MIS ALUMNOS VIERON RESULTADOS ESPECTACULARES EN LOS PRIMEROS 7 DÍAS APLICANDO EL PLAN A",
+    description: "",
+    options: ["¡YO TAMBIÉN QUIERO ESOS RESULTADOS!"],
+    elements: {
+      bigNumber: "91%",
+      profileComplete: "98%",
+      testimonialImage: "https://comprarplanseguro.shop/wp-content/uploads/2025/06/JOSE-DEP-CANVA.png",
+      additionalImage1: "https://comprarplanseguro.shop/wp-content/uploads/2025/06/3-JUAN-DEP-CANVA.png",
+      additionalImage2: "https://comprarplanseguro.shop/wp-content/uploads/2025/06/2-LUIS-DEP-CANVA.png"
+    },
+  },
+  {
     id: 13,
     question: "TU PLAN A - RECUPERACIÓN RÁPIDA DE 21 DÍAS",
     description: "Desarrollado específicamente para tu caso, basado en tus respuestas.",
